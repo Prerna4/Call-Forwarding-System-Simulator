8cacdd1b5863540246d6518eeff9f80b92d4aae1
Prerna4 <81554035+Prerna4@users.noreply.github.com>
2024-02-12 19:59:53 +0530

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "../inc/library.h"
+
+int main()
+{
+        //initializeSystem();
+        log_message(INFO, "Entering to the System");
+        printf("\n---------------------------------------------------------\n");
+        printf("\n.............WELCOME TO CALL FORWARDING SYSTEM...........\n");
+        printf("\n---------------------------------------------------------\n");
+        menu();
+}
